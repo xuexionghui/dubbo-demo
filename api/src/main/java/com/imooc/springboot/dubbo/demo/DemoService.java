310e2cc41b8c2ac1bfd00cdcd3db0c9d3804e584
xuexionghui <413669152@qq.com>
2018-09-19 11:37:31 +0800

    添加注明DemoService的作用

--- api/src/main/java/com/imooc/springboot/dubbo/demo/DemoService.java
+++ api/src/main/java/com/imooc/springboot/dubbo/demo/DemoService.java
@@ -1,5 +1,11 @@
 package com.imooc.springboot.dubbo.demo;
 
 public interface DemoService {
-    String sayHello(String name);
+	/*
+	 * 这是一个接口
+	 * 作用：
+	 *   供consumer在远程消费provider的时候，调用这个接口，从而调用实现类
+	 */
+    //String sayHello(String name);
+	 String  sayHello(String name);
 }
