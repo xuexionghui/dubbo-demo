6743502cb09804bbf77d799858d1627a87dea1c2
xuexionghui <413669152@qq.com>
2018-09-19 11:41:25 +0800

    1

--- api/src/main/java/com/imooc/springboot/dubbo/demo/DemoService.java
+++ api/src/main/java/com/imooc/springboot/dubbo/demo/DemoService.java
@@ -6,6 +6,6 @@ public interface DemoService {
 	 * 作用：
 	 *   供consumer在远程消费provider的时候，调用这个接口，从而调用实现类
 	 */
-    //String sayHello(String name);
+    //String sayHello(String name);11
 	 String  sayHello(String name);
 }
